74ca0779af33da171a726419ce50aba8d7a6a08b
Cedric Mark Solano <162860257+cedsolano@users.noreply.github.com>
2024-04-17 22:26:04 +0800

    Update less5_code1.py

--- Lesson5/less5_code1.py
+++ Lesson5/less5_code1.py
@@ -3,7 +3,7 @@ Problem:
     Write a Python program to create a list of tuples having first element as the number and second element as the square of the number. 
     Input: list = [1, 2, 3] Output: [(1, 1), (2, 4), (3, 9)]
 '''
-
+VARGAS:
 def go_back_to_lesson5_menu():
     input('\nPress any key to go back to the Lesson 5 menu...')
 
@@ -21,3 +21,18 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+SOLANO:
+
+def create_tuples_list(input_list):
+    # Create a list of tuples where the first element is the number and the second element is its square
+    tuples_list = [(num, num ** 2) for num in input_list]
+    return tuples_list
+# Input list
+input_list = [1, 2, 3]
+# Create list of tuples
+result = create_tuples_list(input_list)
+# Print the result
+print("Input:", input_list)
+print("Output:", result)
+
