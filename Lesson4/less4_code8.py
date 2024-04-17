7d294501ef33eb7af5cb63537212c9955b54e3cf
Ranjel272 <162986972+Ranjel272@users.noreply.github.com>
2024-04-17 16:50:29 -0700

    Update less4_code8.py

--- Lesson4/less4_code8.py
+++ Lesson4/less4_code8.py
@@ -1,1 +1,21 @@
-# pakilagay po dito yung code sa 8th question sa Lesson 4 thank u!
+Problem:
+Write a program that asks the user for an integer and creates a list that consists of the factors of that integer
+
+SOURCE CODE
+# Ask the user for an integer
+num = int(input("Enter an integer: "))
+
+# Initialize an empty list to store factors
+factors = []
+
+# Iterate through numbers from 1 to the input number
+for i in range(1, num + 1):
+    # Check if the current number is a factor of the input number
+    if num % i == 0:
+        # If it is, add it to the list of factors
+        factors.append(i)
+
+# Print the list of factors
+print("Factors of", num, "are:", factors)
+
+
