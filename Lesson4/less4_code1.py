7fa9050e816a92c606bb2320b9add43c55be7243
Cedric Mark Solano <162860257+cedsolano@users.noreply.github.com>
2024-04-18 08:55:57 +0800

    Update less4_code1.py

--- Lesson4/less4_code1.py
+++ Lesson4/less4_code1.py
@@ -1,1 +1,19 @@
-# pakilagay po dito yung code sa 1st question sa Lesson 4 thank u!
+Define the variables x and y as lists of numbers 
+x=[1, 2, 3, 4, 5] 
+y=[11, 12, 13, 14, 15] 
+z=(21, 22, 23, 24, 25)  
+A.	What is the value of 3*x?  - Value of 3*x: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+B.	What is the value of x+y? - Value of x+y: [1, 2, 3, 4, 5, 11, 12, 13, 14, 15]
+C.	What is the value of x-y?  - Value of x-y: ERROR
+D.	What is the value of x[1]? - Value of x[1]: 2
+E.	What is the value of x[0]? - Value of x[0]: 1
+F.	What is the value of x[-1]? - Value of x[-1]: 5
+G.	What is the value of x[:]? - Value of x[:]: [1, 2, 3, 4, 5] 
+H.	What is the value of x[2:4]? - Value of x[2:4]: [3, 4]
+I.	What is the value of x[1:4:2]? - Value of x[1:4:2]: [2, 4]
+J.	What is the value of x[:2]? - Value of x[:2]: [1, 2]
+K.	What is the value of x[::2]? - Value of x[::2]: [1, 3, 5]
+L.	What is the result of the following two expressions? 
+•	x[3]=8 - x[3]=8
+•	print x - print x: [1, 2, 3, 8, 5]
+
