c0026a6f77eafa81f2eb7ca946aa6c3167474ff4
Ranyeelll <163998069+Ranyeelll@users.noreply.github.com>
2024-04-18 10:46:37 +0800

    Update less4_code1.py

--- Lesson4/less4_code1.py
+++ Lesson4/less4_code1.py
@@ -21,3 +21,40 @@ L.	What is the result of the following two expressions?
 •	x[3]=8 - x[3]=8
 •	print x - print x: [1, 2, 3, 8, 5]
 '''
+
+
+# ALOVEROS: 
+
+x = [ 1,2,3,4,5]
+y = [11,12,13,14,15,]
+z = [21,22,23,24,25]
+
+a = 3 * x
+b = x + y
+c = [x_val - y_val for x_val, y_val in zip(x,y)]
+d = x[1]
+e = x[0]
+f = x[-1]
+g = x[:]
+h = x[2:4]
+i = x[1:4:2]
+j = x[:2]
+k = x[::2]
+x[3] = 8
+
+
+print("x =",x)
+print("y =",y)
+print("z =",z)
+print("\n3*x =",a)
+print("x+y =",b)
+print("x-y =",c)
+print("x[1] =",d)
+print("x[0] =",e)
+print("x[-1] =",f)
+print("x[:] =",g)
+print("x[2:4] =",h)
+print("x[1:4:2] =",i)
+print("x[:2] =",j)
+print("x[::2] =",k)
+print(f"x ={x}")
