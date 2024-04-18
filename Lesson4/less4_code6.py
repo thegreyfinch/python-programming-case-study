9e1384ab3b9f13288d0f7d9ecb4dc09135c616a7
Ranjel272 <162986972+Ranjel272@users.noreply.github.com>
2024-04-17 18:30:13 -0700

    Update less4_code6.py

--- Lesson4/less4_code6.py
+++ Lesson4/less4_code6.py
@@ -5,22 +5,12 @@ Problem:
 '''
 
 # ESTANISLAO:
-# Ask the user for the number of elements in the list
-num_elements = int(input("Enter the number of elements: "))
+original_list = [1, 1, 2, 3, 4, 3, 0, 0] 
+print("Original List:", original_list)
 
-# Initialize an empty list to store the elements
-my_list = []
-
-# Take input from the user for each element
-for i in range(num_elements):
-    my_list.append(int(input("Enter element {}: ".format(i + 1))))
-
-# Remove repeated items from the list
 unique_list = []
-for item in my_list:
+for item in original_list:
     if item not in unique_list:
         unique_list.append(item)
 
-# Print the unique_list
-print("Original list:", my_list)
-print("List with repeated items removed:", unique_list)
+print("Unique List:", unique_list)
