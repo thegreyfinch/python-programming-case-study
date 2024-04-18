f774dcd1e0e703d72e0e9d0bbcd82b4b5e0f64fe
Ranyeelll <163998069+Ranyeelll@users.noreply.github.com>
2024-04-18 11:28:31 +0800

    Update less4_code6.py

--- Lesson4/less4_code6.py
+++ Lesson4/less4_code6.py
@@ -14,3 +14,13 @@ for item in original_list:
         unique_list.append(item)
 
 print("Unique List:", unique_list)
+
+# ALOVEROS:
+
+list = [1,1,2,3,4,3,0,0]
+print("Original List:",list)
+list2 =[]
+for x in list:
+    if x not in list2:
+        list2.append(x)
+print("FInal List:",list2)
