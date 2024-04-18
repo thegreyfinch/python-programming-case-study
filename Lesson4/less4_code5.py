07b14aa08675dec7cc3295b4d95faf988e7aa986
thegreyfinch <167048356+thegreyfinch@users.noreply.github.com>
2024-04-18 07:35:10 +0000

    modify problem in lesson 5

--- Lesson4/less4_code5.py
+++ Lesson4/less4_code5.py
@@ -1,6 +1,6 @@
 '''
 Problem:
-    Write a program that asks the user for an integer and creates a list that consists of the factors of that integer
+    Write a program that asks the user for an integer and creates a list that consists of the factors of that integer.
 '''
 
 # ESTANISLAO:
