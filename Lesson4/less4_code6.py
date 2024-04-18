9c13bc16338754c535acd22957f4570ae7866def
Cedric Mark Solano <162860257+cedsolano@users.noreply.github.com>
2024-04-18 08:57:20 +0800

    Update less4_code6.py

--- Lesson4/less4_code6.py
+++ Lesson4/less4_code6.py
@@ -1,1 +1,23 @@
-# pakilagay po dito yung code sa 6th question sa Lesson 4 thank u!
+Write a program that asks the user for an integer and creates a list that consists of the factors of that integer
+
+# Function to find factors of a given number
+def find_factors(num):
+    factors = []
+    for i in range(1, num + 1):
+        if num % i == 0:
+            factors.append(i)
+    return factors
+
+# Ask the user for an integer
+try:
+    num = int(input("Enter an integer: "))
+    if num < 1:
+        print("Please enter a positive integer.")
+    else:
+        # Find factors of the given integer
+        factors = find_factors(num)
+        
+        # Print the list of factors
+        print("Factors of", num, "are:", factors)
+except ValueError:
+    print("Invalid input! Please enter a valid integer.")
