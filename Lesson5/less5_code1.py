05f0b7e446d052be01470ee68aa6f39aaedf9c2a
thegreyfinch <167048356+thegreyfinch@users.noreply.github.com>
2024-04-19 02:49:14 +0000

    add title & problem in less 5 code 1

--- Lesson5/less5_code1.py
+++ Lesson5/less5_code1.py
@@ -1,25 +1,41 @@
+class ListOfTupleGenerator:
+    def __init__(self):
+        self.num_list = [1, 2, 3] # iniialize the list inside __init__() method
+        self.tuple_list = []  # list property that stores the tuples
+    
+    def display_title_of_problem(self):
+        print('Title: Generate Tuples with Numbers and Their Squares.')
+    
+    def display_problem(self):
+        print(
 '''
 Problem:
     Write a Python program to create a list of tuples having first element as the number and second element as the square of the number. 
     Input: list = [1, 2, 3] Output: [(1, 1), (2, 4), (3, 9)]
 '''
-
-# Final Source Code:
-
-def go_back_to_lesson5_menu():
-    input('\nPress any key to go back to the Lesson 5 menu...')
-
-def main():
-    print('Generate Tuples with Numbers and Their Squares.\n')
-    num_list = [1, 2, 3]
-    tuple_list = []
-    for num in num_list:
-        tuple_list.append((num, num**2))
-    print(tuple_list)
-
-    # prompt the user to go back to the menu for lesson 5
-    go_back_to_lesson5_menu()
+        )
+        print('Answers: \n')
     
+    def generate_list_of_tuples(self):
+        for num in self.num_list:
+            self.tuple_list.append((num, num**2))
+    
+    def display_list_of_tuples(self):
+        print('List of Tuples: ')
+        print(self.tuple_list)
+    
+    def go_back_to_lesson5_menu(self):
+        input('\nPress any key to go back to the Lesson 5 menu...')
+
+class Program:
+    def main(self):
+        tuple_generator = ListOfTupleGenerator()
+        tuple_generator.display_title_of_problem()
+        tuple_generator.display_problem()
+        tuple_generator.generate_list_of_tuples()
+        tuple_generator.display_list_of_tuples()
+        tuple_generator.go_back_to_lesson5_menu()
 
 if __name__ == "__main__":
-    main()
+    program = Program()
+    program.main()
