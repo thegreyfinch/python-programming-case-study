ac750f44ab7a95a588d877293471806b6e63e107
thegreyfinch <167048356+thegreyfinch@users.noreply.github.com>
2024-04-19 07:50:53 +0000

    add title & problem in code 6 less 5

--- Lesson4/less4_code6.py
+++ Lesson4/less4_code6.py
@@ -1,23 +1,37 @@
 '''
+
+'''
+class ListCleaner:
+    def __init__(self, original_list):
+        self.original_list = original_list
+        self.unique_list = []
+    
+    def display_title_of_problem(self):
+        print('Title: Removing Repeated Items in the List')
+    
+    def display_problem(self):
+        print(
+'''
 Problem: 
     Write a program that removes any repeated items from a list so that each item appears at most once. 
     For instance, the list [1,1,2,3,4,3,0,0] would become [1,2,3,4,0].
 '''
-class ListCleaner:
-    def __init__(self, input_list):
-        self.input_list = input_list
-        self.clean_list = []
+        )
+        print('Answers: \n')
+    
+    def display_original_list(self):
+        print(f'Original List: {self.original_list}')
 
     def remove_duplicates(self):
-        for x in self.input_list:
-            if x not in self.clean_list:
-                self.clean_list.append(x)
+        for x in self.original_list:
+            if x not in self.unique_list:
+                self.unique_list.append(x)
 
     def get_clean_list(self):
-        return self.clean_list
+        return self.unique_list
 
     def display_clean_list(self):
-        print(f"Clean List : {self.clean_list}")
+        print(f"Unique List : {self.unique_list}")
 
     def go_back_to_lesson4_menu(self):
         input('\nPress any key to go back to the Lesson 4 menu...') 
@@ -28,7 +42,11 @@ class Program:
     def main():
         int_list = [1, 1, 2, 3, 4, 3, 0, 0]
         list_cleaner = ListCleaner(int_list)
+        # Display title and problem
+        list_cleaner.display_title_of_problem()
+        list_cleaner.display_problem()
         list_cleaner.remove_duplicates()
+        list_cleaner.display_original_list()
         list_cleaner.display_clean_list()
         list_cleaner.go_back_to_lesson4_menu()
 
